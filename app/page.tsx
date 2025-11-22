5e6610f589408e8f970fe658026b25d4061fb36c
Rahat7242872 <rahat.hossan72428@gmail.com>
2025-11-22 11:44:06 -0800

    update project

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,8 @@
-
+"use client";
 import Services from "./features/Home/servicesSection";
 import Landing from "./features/Home/Hero";
 import About from "./features/Home/about";
 
-// 👉 SEO Metadata
-export const metadata = {
-  title: "Raddsoft LLC | Business Setup, Web & Mobile App Development, Digital Marketing",
-  description: "",
-  keywords: [
-    "Business Setup",
-    "Payment Gateway",
-    "Web Application Development",
-    "Mobile App Development",
-    "Digital Marketing",
-    "Raddsoft LLC"
-  ],
-  openGraph: {
-    title: "Raddsoft LLC - Business & IT Solutions",
-    description: "Business Setup, Web & Mobile App Development, and Digital Marketing services by Raddsoft LLC.",
-    url: "https://www.raddsoft.com",
-    siteName: "Raddsoft LLC",
-    locale: "en_US",
-    type: "website",
-  },
-  alternates: {
-    canonical: "https://www.raddsoft.com",
-  },
-  robots: {
-    index: true,
-    follow: true,
-  },
-};
 
 export default function Home() {
   return (
